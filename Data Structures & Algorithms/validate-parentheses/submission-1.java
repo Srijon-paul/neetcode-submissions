baa26e1cf59f84e31ed3c0cf7d3f42db21f1f74a
Srijon <srijonpaul003@gmail.com>
2026-08-18 10:27:37 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        int i = 0;
+        while (i < s.length()) {
+            if (s.charAt(i) == '[' || s.charAt(i) == '{' || s.charAt(i) == '(') {
+                st.push(s.charAt(i));
+                i++;
+            } else {
+                if (!st.isEmpty() && s.charAt(i) == ']' && st.peek() == '[') {
+                    i++;
+                    st.pop();
+                } else if (!st.isEmpty() && s.charAt(i) == '}' && st.peek() == '{') {
+                    i++;
+                    st.pop();
+                } else if (!st.isEmpty() && s.charAt(i) == ')' && st.peek() == '(') {
+                    i++;
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (!st.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+}
